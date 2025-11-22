fa8e765041dc30868e8ce4aa3099a46aa50b6bc6
lockwood-ethan <lockwood.ethan@yahoo.com>
2025-11-22 10:28:48 +0000

    post requests are returning jwt tokens, can use jwt tokens to make get requests to protected data

---
+++ src/main/java/com/spring/techblog/models/User.java
@@ -0,0 +1,49 @@
+package com.spring.techblog.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private boolean enabled;
+
+    protected User() {}
+
+    public User(String username, String password, boolean enabled) {
+        this.username = username;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
